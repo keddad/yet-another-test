d509d51b25529d24b08ab77e15fa6f88c76fe3c5
keddad <keddad@yandex.ru>
2022-05-26 21:38:05 +0300

    Additional tests

--- src/test/kotlin/com/testproject/ApplicationTest.kt
+++ src/test/kotlin/com/testproject/ApplicationTest.kt
@@ -1,17 +1,13 @@
 package com.testproject
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import com.testproject.plugins.configureRouting
+import com.testproject.plugins.configureSerialization
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.testproject.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
@@ -71,6 +67,22 @@ class ApplicationTest {
         }
         client.post("/market/deal") {
             setBody("""{"id":0,"amount":8}""")
+            header("Content-Type", ContentType.Application.Json.toString())
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+        }
+    }
+
+    @Test
+    fun testInvalidId() = testApplication {
+        var (shop, account) = loadDb("data.json")
+
+        application {
+            configureRouting(shop, account)
+            configureSerialization()
+        }
+        client.post("/market/deal") {
+            setBody("""{"id":69,"amount":8}""")
             header("Content-Type", ContentType.Application.Json.toString())
         }.apply {
             assertEquals(HttpStatusCode.BadRequest, status)
